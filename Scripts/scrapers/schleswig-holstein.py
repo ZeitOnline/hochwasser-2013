75af0ff363a9e5e95cfd3ed326d3664585c5c0e2
Julian Stahnke <hello@julianstahnke.com>
2013-06-11 11:38:44 +0200

    fix scraper for Schleswig-Holstein (hopefully)

--- Scripts/scrapers/schleswig-holstein.py
+++ Scripts/scrapers/schleswig-holstein.py
@@ -14,16 +14,6 @@ if len(sys.argv) > 1:
 else:
     datestring = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-try:
-    r = requests.get('http://www.umweltdaten.landsh.de/public/hsi/hochwasser.html', timeout=30)
-except Exception, e:
-    print "Timeout"
-    sys.exit()
-    
-if r.status_code != 200:
-    print "Server down"
-    sys.exit()
-
 output_path = os.path.join("_scraped_data", datestring)
 
 if not os.path.exists(output_path):
@@ -37,26 +27,8 @@ csv_writer.writerow([
     "Station",
     "Zeitpunkt",
     "Wasserstand",
-    "Meldestufe",
-    "Meldestufe (Alarm)"
+    "Meldestufe"
 ])
-
-soup = BeautifulSoup(r.text, "lxml")
-
-paragraphs = soup.select("#content p")
-
-alarme = {}
-
-for i, paragraph in enumerate(paragraphs):
-
-    if "Am Pegel " in paragraph.text:
-        raw_text = paragraph.text.encode("utf-8")
-        raw_text = raw_text.replace("Am Pegel ", "")
-        raw_text = raw_text.replace(" voraussichtlich überschritten.", "")
-        raw_text = raw_text.replace("wird die Alarmstufe A", "")
-        
-        station_name, meldestufe = raw_text.split()
-        alarme[station_name] = "vorraussichtlich %s" % meldestufe
 
 try:
     r = requests.get('http://www.umweltdaten.landsh.de/public/hsi/pegelliste.html', timeout=30)
@@ -72,6 +44,8 @@ soup = BeautifulSoup(r.text, "lxml")
 
 rows = soup.select("#ListData .TabBodyList")
 
+expression = re.compile("_a([1-4])_", re.IGNORECASE)
+
 for row in rows:
     tds = row.select("td")
     fluss_name = tds[2].text.split(" / ")[-1]
@@ -84,36 +58,37 @@ for row in rows:
         img = img[0]
         img_src = img["src"].split("_")[-1]
         img_src = img_src.split(".")[0]
-        if img_src == "0":
-            meldestufe = "Niedrigwasser (0)"
-        if img_src == "1":
-            meldestufe = "Mittleres Niedrigwasser (1)"
-        if img_src == "2":
-            meldestufe = "Mittelwasser (2)"
-        if img_src == "3":
-            meldestufe = "Mittelwasser (3)"
-        if img_src == "4":
-            meldestufe = "Mittelwasser (4)"
-        if img_src == "5":
-            meldestufe = "Mittleres Hochwasser (5)"
-        if img_src == "6":
-            meldestufe = "Mittleres Hochwasser (6)"
-        if img_src == "7":
-            meldestufe = "Mittleres Hochwasser (7)"
-        if img_src == "8":
-            meldestufe = "Höchstes Hochwasser (8)"
-    
-    meldestufe_alarm = "-"
-    if station_name in alarme:
-        meldestufe_alarm = alarme[station_name]
-    
+        alarm_match = expression.search(img["src"])
+        if alarm_match:
+            meldestufe = alarm_match.group(1)
+        else:
+            if img_src == "0":
+                meldestufe = "Niedrigwasser (0)"
+            if img_src == "1":
+                meldestufe = "Mittleres Niedrigwasser (1)"
+            if img_src == "2":
+                meldestufe = "Mittelwasser (2)"
+            if img_src == "3":
+                meldestufe = "Mittelwasser (3)"
+            if img_src == "4":
+                meldestufe = "Mittelwasser (4)"
+            if img_src == "5":
+                meldestufe = "Mittleres Hochwasser (5)"
+            if img_src == "6":
+                meldestufe = "Mittleres Hochwasser (6)"
+            if img_src == "7":
+                meldestufe = "Mittleres Hochwasser (7)"
+            if img_src == "8":
+                meldestufe = "Höchstes Hochwasser (8)"
+            if img_src == "9":
+                meldestufe = "Höchstes Hochwasser (9)"
+
     csv_writer.writerow([
         fluss_name.encode("UTF-8"),
         station_name.encode("UTF-8"),
         zeitpunkt.encode("UTF-8"),
         wasserstand.encode("UTF-8"),
-        meldestufe.encode("UTF-8"),
-        meldestufe_alarm.encode("UTF-8"),
+        meldestufe
     ])
 
 output_file.close()
